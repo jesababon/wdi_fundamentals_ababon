e0e82540717a14a2eee32752bb7631e73998a810
Jes Ababon <jesababon@gmail.com>
2018-04-16 00:02:18 -0400

    finished up game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -33,16 +33,29 @@ var checkForMatch = function () {
 		{alert("Sorry. Try Again.");}
 };
 
-var flipCard = function(cardId) {
-	cardsInPlay.push(cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute("data-id");
 	console.log ("User flipped " + cards[cardId].rank);
 	console.log ("User flipped " + cards[cardId].cardImage);
-	console.log ("User flipped " + cards[cardId].suit);
-	if (cardsInPlay.length === 2) {
+	console.log ("User fllipped " + cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
+	
+
+	this.setAttribute("src", cards[cardId].cardImage);
+		if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 	};
 
-flipCard(0);
-flipCard(1);
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+};
+}
+
+createBoard();
 
