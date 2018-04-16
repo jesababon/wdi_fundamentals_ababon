1bc100a3bd19b66649a6f9bf8d72e865da9a17ba
Jes Ababon <jesababon@gmail.com>
2018-04-15 21:21:12 -0400

    added rank suit image

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,48 @@
 console.log("Up and running!");
 
-var cards = [ "queen" , "queen" , "king" , "king"]
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage:"images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-hearts.png"
+}
+];
 
 var cardsInPlay = [];
 
-var cardOne = [0];
-cardsInPlay.push("cardOne");
-console.log("User flipped queen.");
 
-var cardTwo = [2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king.");
+var checkForMatch = function () {
+	if(cardsInPlay[0] === cardsInPlay[1]) 
+		{alert("Cool, you found a match!");} 
+	else 
+		{alert("Sorry. Try Again.");}
+};
 
+var flipCard = function(cardId) {
+	cardsInPlay.push(cards[cardId].rank);
+	console.log ("User flipped " + cards[cardId].rank);
+	console.log ("User flipped " + cards[cardId].cardImage);
+	console.log ("User flipped " + cards[cardId].suit);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
+	};
 
-if (cardOne === cardTwo) {
-alert("It's a match!");
-} else {
-	alert("Sorry. Try Again.");
-}
+flipCard(0);
+flipCard(1);
+
